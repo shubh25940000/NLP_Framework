440db020bc17f0b51e80cc06da0e4af377672274
shubh25940000 <shubh2594@gmail.com>
2022-04-22 19:36:13 +0530

    Created basic_cleanups.py

---
+++ basic_cleanups.py
@@ -0,0 +1,87 @@
+# import packages
+
+import pandas as pd
+import numpy as np
+import nltk
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+class basic_cleanups:
+    def __init__(self, source, url = None, file_name = None, stem = 'stemmer'):
+        self.source = source
+        self.url = url
+        self.file_name = file_name
+        self.stem = stem
+
+    def source_data(self):
+        if self.source == 'WEB':
+            from bs4 import BeautifulSoup
+            import requests
+            page = requests.get(self.url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            k = dict()
+            result = soup.find_all('p')
+            for i in range(1, len(result)):
+                k[i] = result[i].get_text()
+            df = pd.DataFrame(k.items())
+            df.rename(columns = {0: 'Id', 1: 'Text'}, inplace = True)
+            return df
+        elif self.source == 'LOCAL':
+            df = pd.read_csv(self.file_name)
+            return df
+    def clean_dataset(self, df, col='Text'):
+        #Assuming the data is sourced from wikipedia
+            #1. Remove numbers inside square brackets
+            #2. Remove '\n' and '\t'
+
+        if self.source == 'WEB':
+            def sub(string):
+                import re
+                p1 = re.compile("\[\d*\]")
+                p2 = re.compile("(\n)|(\t)")
+                return re.sub(p1, '', re.sub(p2, '', string))
+            df['Text_cleaned'] = df['Text'].apply(sub)
+            return df
+        elif self.source == 'FILE':
+            def sub(string):
+                import re
+                p1 = re.compile("\[\d*\]")
+                p2 = re.compile("(\n)|(\t)")
+                return re.sub(p1, '', re.sub(p2, '', string))
+            df['Text_cleaned'] = df[col].apply(sub)
+            return df
+    def lemma(self, df, col = 'Text'):
+        from nltk import NLTKWordTokenizer
+        if self.stem == 'stemmer':
+            from nltk.stem import PorterStemmer
+            TOK = NLTKWordTokenizer()
+            stem = PorterStemmer()
+            def stemming(string):
+                x = TOK.tokenize(string)
+                x = ' '.join([stem.stem(i).lower() for i in x])
+                return x
+            df['Stemmed'] = df[col].apply(stemming)
+            return df
+        elif self.stem == 'lemmatize':
+            from nltk.stem import WordNetLemmatizer
+            TOK = NLTKWordTokenizer()
+            lemma = WordNetLemmatizer()
+            def lemmatize(string):
+                x = TOK.tokenize(string)
+                t = nltk.pos_tag(x)
+                j = []
+                # Using POS Tags for precise lemmatization
+                for word, tag in t:
+                    if tag.startswith("NN"):
+                        j.append(lemma.lemmatize(word, pos='n').lower())
+                    elif tag.startswith('VB'):
+                        j.append(lemma.lemmatize(word, pos='v').lower())
+                    elif tag.startswith('JJ'):
+                        j.append(lemma.lemmatize(word, pos='a').lower())
+                    else:
+                        j.append(word.lower())
+                x = ' '.join(j)
+                return x
+            df['Lemmatized'] = df[col].apply(lemmatize)
+            return df
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #import packages
 
+import basic_cleanups
 import basic_analysis
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    x = basic_analysis.basic_analysis(source ='WEB', url ='https://en.wikipedia.org/wiki/Barack_Obama')
+    x = basic_cleanups.basic_cleanups(source ='WEB', url ='https://en.wikipedia.org/wiki/Barack_Obama', stem = 'lemmatize')
+    y = basic_analysis.basic_analysis(source ='WEB', url ='https://en.wikipedia.org/wiki/Barack_Obama', stem = 'lemmatize')
     df = x.source_data()
     df = x.clean_dataset(df)
     df = x.lemma(df)
-    print(df.head())
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    vec = y.basic_info(df)
+    df.to_csv('Output_check.csv', index = False)
+    print(vec)
