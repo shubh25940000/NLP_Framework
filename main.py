b0561b183510131cdb3fc31dfaf4fc16ae2f851e
shubh25940000 <shubh2594@gmail.com>
2022-04-22 11:14:34 +0530

    1st Commit; Added Main.py and a initial analysis py file.

---
+++ main.py
@@ -0,0 +1,13 @@
+#import packages
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
